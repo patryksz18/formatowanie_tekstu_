0e15ec3b37317362fdb26e940a18c37e5bf02a49
byken <byken@KOMPDODUPY>
2024-12-06 16:43:01 +0100

    Dodaj pliki projektów.

---
+++ formatowanie_tekstu_/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace formatowanie_tekstu_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
